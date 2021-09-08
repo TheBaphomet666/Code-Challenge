480c34ea4b960953566d45f446ac2d5df821e6b6
Oscar Pinto <oscar.pinto@payulatam.com>
2021-09-07 22:36:15 -0500

    first commit

--- src/main/java/com/example/codechallenge/CodeChallengeApplication.java
+++ src/main/java/com/example/codechallenge/CodeChallengeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CodechallengeApplication {
+public class CodeChallengeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CodechallengeApplication.class, args);
+		SpringApplication.run(CodeChallengeApplication.class, args);
 	}
 
 }
